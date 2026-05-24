61c9baa4378b1af2914c820bdbbd70858b77fb43
devops4sure <devops4sure@gmail.com>
2026-05-23 19:50:04 -0700

    had some new additions

--- terraform/modules/tf_state_backend/output.tf
+++ terraform/modules/tf_state_backend/output.tf
@@ -1,19 +1,19 @@
-output "terraform_state_bucket_name" {
-  description = "Name of the Terraform state S3 bucket"
-  value       = aws_s3_bucket.terraform_state.bucket
-}
-
-output "terraform_state_bucket_arn" {
-  description = "ARN of the Terraform state S3 bucket"
-  value       = aws_s3_bucket.terraform_state.arn
-}
-
-output "terraform_locks_table_name" {
-  description = "Name of the DynamoDB lock table"
-  value       = aws_dynamodb_table.terraform_locks.name
-}
-
-output "terraform_locks_table_arn" {
-  description = "ARN of the DynamoDB lock table"
-  value       = aws_dynamodb_table.terraform_locks.arn
-}
+# output "terraform_state_bucket_name" {
+#   description = "Name of the Terraform state S3 bucket"
+#   value       = aws_s3_bucket.terraform_state.bucket
+# }
+#
+# output "terraform_state_bucket_arn" {
+#   description = "ARN of the Terraform state S3 bucket"
+#   value       = aws_s3_bucket.terraform_state.arn
+# }
+#
+# output "terraform_locks_table_name" {
+#   description = "Name of the DynamoDB lock table"
+#   value       = aws_dynamodb_table.terraform_locks.name
+# }
+#
+# output "terraform_locks_table_arn" {
+#   description = "ARN of the DynamoDB lock table"
+#   value       = aws_dynamodb_table.terraform_locks.arn
+# }
