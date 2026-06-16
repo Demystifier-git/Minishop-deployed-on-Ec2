dfd2c3aa20462b32f9d5298ac3a167ca1548057d
devops4sure <devops4sure@gmail.com>
2026-06-16 11:17:27 -0700

    new update on asg config

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -17,3 +17,13 @@ resource "aws_security_group_rule" "prometheus_to_otel" {
   security_group_id        = module.backend_asg_sg.security_group_id
   source_security_group_id = module.web_sg.security_group_id
 }
+
+resource "aws_security_group_rule" "alb_to_backend" {
+  type                     = "ingress"
+  from_port                = 8000
+  to_port                  = 8000
+  protocol                 = "tcp"
+
+  security_group_id        = module.backend_asg_sg.security_group_id
+  source_security_group_id = module.alb_sg.security_group_id
+}
