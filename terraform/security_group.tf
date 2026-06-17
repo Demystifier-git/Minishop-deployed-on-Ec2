e235b9ea51c4dc5d0a0cc6337e60bb50e509532a
devops4sure <devops4sure@gmail.com>
2026-06-17 11:22:40 -0700

    new update

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -28,12 +28,12 @@ resource "aws_security_group_rule" "alb_to_backend" {
   source_security_group_id =  module.lb_ssl.lb_security_group_id
 }
 
-resource "aws_security_group_rule" "otel_to_lb" {
+resource "aws_security_group_rule" "alb_to_otel" {
   type                     = "ingress"
   from_port                = 8889
   to_port                  = 8889
   protocol                 = "tcp"
 
-  security_group_id        = module.backend_asg_sg.security_group_id
+  security_group_id        = module.otel_sg.security_group_id
   source_security_group_id = module.lb_ssl.lb_security_group_id
 }
